c6cffc4df42605d9594b9faa2ca1cc28928fcfc4
AmsTaFFix <amstaffix@gmail.com>
2014-09-10 12:31:01 +0400

    Первый коммит

---
+++ app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('EVEInDust.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'EVEInDust'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
